143cfbe15d4446e81f71ca71d3d5cbacf5c11dc8
86wyc <blackbell.wyc@proton.me>
2026-06-09 09:52:01 +0800

    test push

---
+++ erb.js
@@ -0,0 +1,29 @@
+// var exercise = [
+//     "F",
+//     "E",
+//     "B",
+//     "C",
+//     "A",
+//     "D"
+// ];
+
+// var exercise1 = exercise;
+// var exercise3 = Array.from(exercise);
+// exercise.sort();
+// console.log(exercise, exercise1);
+
+
+// console.log(exercise3);
+
+var num = [
+    3,
+    5,
+    4,
+    2,
+    1
+];
+
+var new_num = Array.from(num);
+
+console.log("new_num.sort()",  new_num.sort());
+console.log("num", num);
