20aff3365779f8bd3cc8ed83756fb802bc0cc645
86wyc <blackbell.wyc@proton.me>
2026-06-11 18:09:42 +0800

    End of third day (Restructured dir).

---
+++ erb.js
@@ -0,0 +1,10 @@
+function getAvg(name, ...assignment) {
+    let avgmark = 0;
+    for (i in assignment) {
+        avgmark += i;
+    }
+    return console.log(name + ' average mark : ' + (avgmark / (getAvg.length)));
+}
+
+getAvg('Peter', 10, 20, 30);
+getAvg('Mary', 50, 20, 30, 100);
